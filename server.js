2014bff6da6f8ec91a912039ccf80b1af8315eb0
macas5 <mkalinauskas1993@gmail.com>
2020-08-11 08:10:59 -0700

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'https://agile-hollows-89259.herokuapp.com/',
       user : 'macas5',
       password : 'asdzxc',
       database : 'smart-brain'
