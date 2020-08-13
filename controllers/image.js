4f29ba924919922f84d518e00f90fee850c7a40c
macas5 <mkalinauskas1993@gmail.com>
2020-08-13 07:56:21 -0700

    Hidden API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: process.env(CLARIFAI_KEY)
+    apiKey: process.env.CLARIFAI_KEY
    });
 
 const handleApiCall = (req, res) => {
