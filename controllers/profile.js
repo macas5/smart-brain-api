70fc242d754b6f66904b43e4ccd89d0472f87fc3
macas5 <mkalinauskas1993@gmail.com>
2020-08-13 08:13:15 -0700

    Cleaned up the code

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,14 +1,14 @@
-const handleProfileGet = (req, res, db) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id}).then(user => {
-        if (user.length) {
-            res.json(user[0]);
-        } else {
-            res.status(400).json('No such user');
-        };
-    });
+const handleProfileGet = (db) => (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id}).then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('No such user');
+    };
+  });
 }
 
 module.exports = {
-    handleProfileGet
+  handleProfileGet
 }
--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex ({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
-  });
+  }
+});
 
 const app = express();
 
@@ -26,9 +26,9 @@ app.use(cors());
 app.get('/', (req, res) => {res.send('It is working!')});
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
-app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
-app.put('/image', (req, res) => image.handleImage(req, res, db));
-app.post('/imageurl', (req, res) => image.handleApiCall(req, res, db));
+app.get('/profile/:id', profile.handleProfileGet(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', image.handleApiCall());
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App is running on port${process.env.PORT}`);
